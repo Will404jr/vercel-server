1d9ecbdd4553042dfe606553a1c5ef72684848c4
will jr <wjr46269@gmail.com>
2024-05-15 15:03:13 +0200

    changes

--- auth/routers/login.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getAlllogins } = require("../controllers/login.controller");
-
-const loginRouter = express.Router();
-
-loginRouter.post("/api/login", getAlllogins);
-
-module.exports = loginRouter;
